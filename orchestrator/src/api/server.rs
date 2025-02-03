c08aecef00c5d1eb326d4914a85f91b51683e4ff
Jannik Straube <info@jannik-straube.de>
2025-02-03 19:18:21 +0100

    add missing api_key_mw

--- orchestrator/src/api/server.rs
+++ orchestrator/src/api/server.rs
@@ -2,9 +2,13 @@ use crate::api::routes::nodes::nodes_routes;
 use crate::api::routes::task::tasks_routes;
 use crate::api::routes::{heartbeat::heartbeat_routes, metrics::metrics_routes};
 use crate::store::core::StoreContext;
+use actix_web::middleware::{Compress, NormalizePath, TrailingSlash};
 use actix_web::{middleware, web::Data, App, HttpServer};
+use actix_web::{web, HttpResponse};
 use anyhow::Error;
 use log::info;
+use serde_json::json;
+use shared::security::api_key_middleware::ApiKeyMiddleware;
 use shared::security::auth_signature_middleware::{ValidateSignature, ValidatorState};
 use std::sync::Arc;
 
@@ -16,6 +20,7 @@ pub async fn start_server(
     host: &str,
     port: u16,
     store_context: Arc<StoreContext>,
+    admin_api_key: String,
 ) -> Result<(), Error> {
     info!("Starting server at http://{}:{}", host, port);
     let app_state = Data::new(AppState { store_context });
@@ -26,14 +31,24 @@ pub async fn start_server(
             .with_validator(move |address| node_store_clone.get_node(address).is_some()),
     );
 
+    let api_key_middleware = Arc::new(ApiKeyMiddleware::new(admin_api_key));
+
     HttpServer::new(move || {
         App::new()
             .app_data(app_state.clone())
             .wrap(middleware::Logger::default())
+            .wrap(Compress::default())
+            .wrap(NormalizePath::new(TrailingSlash::Trim))
             .service(heartbeat_routes().wrap(ValidateSignature::new(validator_state.clone())))
-            .service(nodes_routes())
-            .service(tasks_routes())
-            .service(metrics_routes())
+            .service(nodes_routes().wrap(api_key_middleware.clone()))
+            .service(tasks_routes().wrap(api_key_middleware.clone()))
+            .service(metrics_routes().wrap(api_key_middleware.clone()))
+            .default_service(web::route().to(|| async {
+                HttpResponse::NotFound().json(json!({
+                    "success": false,
+                    "error": "Resource not found"
+                }))
+            }))
     })
     .bind((host, port))?
     .run()
