5bb0fd8eb80cbf6d26fb2f51138f5b701ce02581
JannikSt <JannikSt@users.noreply.github.com>
2025-02-06 16:53:32 +0100

    release v.0.1.4 (#113)
    * Feature/disable ejection (#111)
    
    * support ability to restore metrics via orchestrator (#112)

--- orchestrator/src/api/routes/metrics.rs
+++ orchestrator/src/api/routes/metrics.rs
@@ -1,10 +1,22 @@
 use crate::api::server::AppState;
 use actix_web::{
-    web::{self, get, Data},
+    web::{self, delete, get, post, Data, Path},
     HttpResponse, Scope,
 };
+use serde::Deserialize;
 use serde_json::json;
 
+#[derive(Deserialize)]
+struct ManualMetricEntry {
+    label: String,
+    value: f64,
+}
+
+#[derive(Deserialize)]
+struct DeleteMetricRequest {
+    label: String,
+    address: String,
+}
 async fn get_metrics(app_state: Data<AppState>) -> HttpResponse {
     let metrics = app_state
         .store_context
@@ -13,6 +25,43 @@ async fn get_metrics(app_state: Data<AppState>) -> HttpResponse {
     HttpResponse::Ok().json(json!({"success": true, "metrics": metrics}))
 }
 
+async fn get_all_metrics(app_state: Data<AppState>) -> HttpResponse {
+    let metrics = app_state.store_context.metrics_store.get_all_metrics();
+    HttpResponse::Ok().json(json!({"success": true, "metrics": metrics}))
+}
+
+// for potential backup restore purposes
+async fn create_metric(
+    app_state: Data<AppState>,
+    metric: web::Json<ManualMetricEntry>,
+) -> HttpResponse {
+    app_state
+        .store_context
+        .metrics_store
+        .store_manual_metrics(metric.label.clone(), metric.value);
+    HttpResponse::Ok().json(json!({"success": true}))
+}
+
+async fn delete_metric(
+    app_state: Data<AppState>,
+    task_id: Path<String>,
+    body: web::Json<DeleteMetricRequest>,
+) -> HttpResponse {
+    let success =
+        app_state
+            .store_context
+            .metrics_store
+            .delete_metric(&task_id, &body.label, &body.address);
+
+    HttpResponse::Ok().json(json!({
+        "success": success
+    }))
+}
+
 pub fn metrics_routes() -> Scope {
-    web::scope("/metrics").route("", get().to(get_metrics))
+    web::scope("/metrics")
+        .route("", get().to(get_metrics))
+        .route("/all", get().to(get_all_metrics))
+        .route("", post().to(create_metric))
+        .route("/{task_id}", delete().to(delete_metric))
 }
