4026d7495887565fc654610d072041e814122000
JannikSt <JannikSt@users.noreply.github.com>
2025-02-06 16:43:52 +0100

    support ability to restore metrics via orchestrator (#112)

--- orchestrator/src/store/domains/metrics_store.rs
+++ orchestrator/src/store/domains/metrics_store.rs
@@ -37,6 +37,7 @@ impl MetricsStore {
             } else {
                 entry.key.task_id
             };
+
             let cleaned_label = self.clean_label(&entry.key.label);
             let redis_key = format!(
                 "{}:{}:{}",
@@ -50,28 +51,10 @@ impl MetricsStore {
                 sender_address.to_string()
             };
 
-            let existing_value: Option<String> = con.hget(&redis_key, &address).unwrap_or(None);
-            let should_update = match existing_value {
-                Some(val) => {
-                    // Only check for max value on dashboard-progress metrics to maintain dashboard integrity
-                    if entry.key.label.contains("dashboard-progress") {
-                        match (val.parse::<f64>(), entry.value) {
-                            (Ok(old_val), new_val) => new_val > old_val,
-                            _ => true,
-                        }
-                    } else {
-                        true
-                    }
-                }
-                None => true,
-            };
-
-            if should_update {
-                if let Err(err) =
-                    con.hset(redis_key, address, entry.value.to_string()) as RedisResult<()>
-                {
-                    error!("Could not update metric value in redis: {}", err);
-                }
+            if let Err(err) =
+                con.hset(redis_key, address, entry.value.to_string()) as RedisResult<()>
+            {
+                error!("Could not update metric value in redis: {}", err);
             }
         }
     }
