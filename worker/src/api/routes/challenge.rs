9557b2f104372924a63f7128e6af6f47c1268199
Matthew Di Ferrante <mattdf@users.noreply.github.com>
2025-02-12 12:18:09 +0100

    make app_state unused

--- worker/src/api/routes/challenge.rs
+++ worker/src/api/routes/challenge.rs
@@ -3,19 +3,14 @@ use actix_web::{
     web::{self, post, Data},
     HttpResponse, Scope,
 };
-use log::info;
 use shared::models::api::ApiResponse;
 use shared::models::challenge::calc_matrix;
 use shared::models::challenge::ChallengeRequest;
 
 pub async fn handle_challenge(
     challenge: web::Json<ChallengeRequest>,
-    app_state: Data<AppState>,
+    _app_state: Data<AppState>,
 ) -> HttpResponse {
-    info!(
-        "Wallet: {:?}",
-        app_state.node_wallet.wallet.default_signer().address()
-    );
     let result = calc_matrix(&challenge);
 
     let response = ApiResponse::new(true, result);
