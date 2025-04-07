747d4ad65624ea362ebe46f1b2b312f5cdc57c22
Jannik Straube <info@jannik-straube.de>
2025-04-07 17:34:16 +0200

    increase min storage to 1tb

--- worker/src/checks/hardware/hardware_check.rs
+++ worker/src/checks/hardware/hardware_check.rs
@@ -69,11 +69,11 @@ impl HardwareChecker {
             issue_tracker.add_issue(IssueType::InsufficientMemory, "Minimum 8GB RAM required");
         }
 
-        if storage_gb < 100 {
-            // 100GB minimum
+        if storage_gb < 1000 {
+            // 1000GB minimum
             issue_tracker.add_issue(
                 IssueType::InsufficientStorage,
-                "Minimum 100GB storage required",
+                "Minimum 1000GB storage required",
             );
         }
 
