ff34ad3e229c2ff5c6e65e84da8317fa43b297e2
Jannik Straube <info@jannik-straube.de>
2025-06-05 09:11:59 +1000

    better metrics changed comaprison

--- crates/orchestrator/src/metrics/webhook_sender.rs
+++ crates/orchestrator/src/metrics/webhook_sender.rs
@@ -28,6 +28,28 @@ impl MetricsWebhookSender {
         }
     }
 
+    pub fn metrics_changed(
+        metrics: &HashMap<String, f64>,
+        last_sent_metrics: &HashMap<String, f64>,
+    ) -> bool {
+        if metrics.len() != last_sent_metrics.len() {
+            return true;
+        }
+
+        // FP imprecision fix
+        const EPSILON: f64 = 1e-10;
+
+        for (key, value) in metrics {
+            match last_sent_metrics.get(key) {
+                None => return true,
+                Some(last_value) if (last_value - value).abs() > EPSILON => return true,
+                _ => continue,
+            }
+        }
+
+        false
+    }
+
     pub async fn run(&mut self) -> Result<()> {
         let mut interval = interval(Duration::from_secs(15));
         loop {
@@ -37,7 +59,7 @@ impl MetricsWebhookSender {
                 .metrics_store
                 .get_aggregate_metrics_for_all_tasks();
 
-            if metrics != self.last_sent_metrics {
+            if Self::metrics_changed(&metrics, &self.last_sent_metrics) {
                 info!("Sending {} metrics via webhook", metrics.len());
                 for plugin in &self.webhook_plugins {
                     let _ = plugin
@@ -48,5 +70,24 @@ impl MetricsWebhookSender {
                 self.last_sent_metrics = metrics.clone();
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_metrics_changed() {
+        let mut metrics = HashMap::new();
+        metrics.insert("test_metric".to_string(), 1.0);
+        metrics.insert("metric_2".to_string(), 2.0);
+
+        let mut last_sent_metrics = HashMap::new();
+        last_sent_metrics.insert("metric_2".to_string(), 2.0);
+        last_sent_metrics.insert("test_metric".to_string(), 1.0);
+
+        let metrics_changed = MetricsWebhookSender::metrics_changed(&metrics, &last_sent_metrics);
+        assert!(!metrics_changed);
     }
 }
