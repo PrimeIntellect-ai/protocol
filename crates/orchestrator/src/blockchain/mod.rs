d6eec2d01e2f60eb2644b3f15776f974e6e203c5
JannikSt <info@jannik-straube.de>
2025-06-19 07:10:56 +0000

    cleanup

--- crates/orchestrator/src/blockchain/mod.rs
+++ crates/orchestrator/src/blockchain/mod.rs
@@ -2,7 +2,6 @@ pub mod event_handlers;
 pub mod event_monitor;
 pub mod handlers;
 pub mod integration;
-pub mod usage_example;
 
 pub use event_handlers::EventHandler;
 pub use event_monitor::{EventMonitor, EventMonitorConfig};
