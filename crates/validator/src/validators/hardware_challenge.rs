f87d5d3f894e72ce30d37c24621475333ecb983c
elizabeth <elizabethjbinks@gmail.com>
2025-07-11 11:33:03 -0400

    remove println

--- crates/validator/src/validators/hardware_challenge.rs
+++ crates/validator/src/validators/hardware_challenge.rs
@@ -30,11 +30,6 @@ impl HardwareChallenge {
             .clone()
             .ok_or_else(|| anyhow::anyhow!("Node {} does not have P2P addresses", node.id))?;
 
-        println!(
-            "Challenging node {} with P2P ID: {} and addresses: {:?}",
-            node.id, p2p_id, p2p_addresses
-        );
-
         // create random challenge matrix
         let challenge_matrix = self.random_challenge(3, 3, 3, 3);
         let challenge_expected = p2p::calc_matrix(&challenge_matrix);
